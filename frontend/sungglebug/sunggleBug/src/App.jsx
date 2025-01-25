1235b070e79d4736a2a54ec86a335cecaa873d6e
2255rishikesh <rishikeshanand319@gmail.com>
2025-01-25 14:13:46 +0530

    changes

--- frontend/sungglebug/sunggleBug/src/App.jsx
+++ frontend/sungglebug/sunggleBug/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router'
+import { Home } from '@mui/icons-material'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +10,12 @@ function App() {
   return (
     <>
       <div>
+      
+       <Routes>
+        <Route path='/' element={<Home/>}></Route>
         
+        </Routes> 
+
       </div>
       
     </>
--- frontend/sungglebug/sunggleBug/src/components/Navbar.jsx
+++ frontend/sungglebug/sunggleBug/src/components/Navbar.jsx
@@ -1,9 +1,20 @@
+import { AppBar, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 const Navbar = () => {
   return (
     <div>
-      
+     <AppBar position='static'>
+        <Toolbar>
+            <Typography variant='h6' marginRight={"auto"}>Blog</Typography>
+            <Link to="/">
+                <Button style={{color:"white"}}>Home</Button>
+            </Link>
+            <Link to="/add">
+                <Button style={{color:"white"}}>Add</Button>
+            </Link>
+        </Toolbar>
+    </AppBar>
     </div>
   )
 }
