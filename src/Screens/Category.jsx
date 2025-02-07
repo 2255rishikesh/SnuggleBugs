8ed368b7ea976ed12678c394933bd0783f199c03
ebin9977 <ebinjaimon59@gmail.com>
2025-02-07 12:10:12 +0530

    newone

--- src/Screens/Category.jsx
+++ src/Screens/Category.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
-function Category() {
+import Cardcomponents from '../Components/Cardcomponents'
+const Category = () => {
   return (
-    <div>Category</div>
+    <div>
+      <center><h1 >Cateories</h1></center>
+      <Cardcomponents id={'ert'} imgurl={"https://momcozy.com/cdn/shop/files/new.jpg?v=1718187078&width=200"} title={"White Noise"}/>
+    </div>
   )
 }
 
