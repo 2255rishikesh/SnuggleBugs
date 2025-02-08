2be9f648294ce0ca5bd3614fa5abaa089ff281f0
ebin9977 <ebinjaimon59@gmail.com>
2025-02-08 16:33:59 +0530

    set

--- src/Components/CategoryComp.jsx
+++ src/Components/CategoryComp.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
-import './Category.css'
-import Cardcomponents from './Cardcomponents'
+// src/Category.js
+import React from 'react';
+import './Category.css';  // Assuming styles are in Category.css
+import Cardcomponents from './Cardcomponents';  // Card component that displays individual items
+import { Link } from 'react-router-dom';  // Using Link for routing
+
 const Category = () => {
   return (
-    <div>
-      <center><h1 >Cateories</h1></center>
-      <div style={{ flex: "1", justifyContent: "flex-start", display: "flex",flexDirection:"row", }}>
+    <div className="category-container">
+      <h1 className="category-title">Categories</h1>
 
-        <Cardcomponents id={'ert'} imgurl={"https://cdn.pixelspray.io/v2/black-bread-289bfa/XUefL6/wrkr/original/mothercare/banner/318_202402290412_MC_CategoryTiles_180x180_babyclothing_100.jpg"} />
-        <Cardcomponents id={'ert'} imgurl={"https://cdn.pixelspray.io/v2/black-bread-289bfa/XUefL6/wrkr/original/mothercare/banner/318_202308290926_MC_CategoryTiles_180x180_baby_care@2x_100.jpg"} />
+      <div className="category-grid">
+        <Link to="/category" className="category-link">
+          <Cardcomponents 
+            id={'ert'}
+            imgurl={"https://cdn.pixelspray.io/v2/black-bread-289bfa/XUefL6/wrkr/original/mothercare/banner/318_202402290412_MC_CategoryTiles_180x180_babyclothing_100.jpg"} 
+          />
+        </Link>
+
+        {/* Add more cards or categories as needed */}
+        <Link to="/category" className="category-link">
+          <Cardcomponents 
+            id={'ert2'}
+            imgurl={"https://cdn.pixelspray.io/v2/black-bread-289bfa/XUefL6/wrkr/original/mothercare/banner/318_202308290926_MC_CategoryTiles_180x180_baby_care@2x_100.jpg"} 
+          />
+        </Link>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
